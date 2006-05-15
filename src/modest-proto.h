2ecc697bfc687863f5a12ae68c562b93741eabd7
Nils Faerber <nils@kernelconcepts.de>
2006-05-15 16:51:09 +0000

    * added GTKdoc compliant documentation
    pmo-trunk-r61

--- src/modest-proto.h
+++ src/modest-proto.h
@@ -17,7 +17,24 @@ enum {
 };
 typedef gint ModestProtoType;
 
+/**
+ * modest_proto_is_valid:
+ * @proto: a string describing the protocol
+ *
+ * checks if proto is a valid protocol
+ *
+ * Returns: TRUE if proto is valid, FALSE otherwise
+ */
 gboolean         modest_proto_is_valid     (const gchar *proto);
+
+/**
+ * modest_proto_type:
+ * @proto: a string describing the protocol
+ *
+ * converts the string proto into a ModestProtoType
+ *
+ * Returns: a valid ModestProtoType corresponding to proto
+ */
 ModestProtoType  modest_proto_type         (const gchar *proto);
 
 #endif /*__MODEST_SERVER_PROTO_H__*/
