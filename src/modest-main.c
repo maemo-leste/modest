d37f9d93ad2b1ff651788fb7eed6abbe4804c13c
Dirk-Jan C. Binnema <dirk-jan.binnema@nokia.com>
2006-07-10 07:13:26 +0000

    * lots of cleanups, removed destructive settings / test install settings   (they can now be done using gconftool) * made the return codes well-defined * added license header
    pmo-trunk-r352

--- src/modest-main.c
+++ src/modest-main.c
@@ -1,5 +1,36 @@
-/* modest-main.c -- part of modest */
+/* Copyright (c) 2006, Nokia Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the Nokia Corporation nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 #include <glib.h>
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
 
 #include "modest-conf.h"
 #include "modest-account-mgr.h"
@@ -10,17 +41,24 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif /*HAVE_CONFIG_H*/
-#include <gtk/gtk.h>
 
-#ifdef IS_HILDON /* Hildon includes */
+#ifdef MODEST_ENABLE_HILDON /* Hildon includes */
 #include <libosso.h>
-#define APPLICATION_DBUS_SERVICE "modest"
-#endif
+#endif /* MODEST_ENABLE_HILDON */
+
+/* return values */
+#define MODEST_ERR_NONE    0
+#define MODEST_ERR_OPTIONS 1
+#define MODEST_ERR_CONF    2
+#define MODEST_ERR_UI      3
+#define MODEST_ERR_HILDON  4
+#define MODEST_ERR_RUN     5
 
 
-static void install_basic_conf_settings (ModestConf *conf);
-static void install_test_account        (ModestConf *conf);
+static gboolean hildon_init (); /* NOP if HILDON is not defined */
 
+static int start_ui (ModestConf *conf, const gchar* mailto, const gchar *cc,
+		     const gchar *bcc, const gchar* subject, const gchar *body);
 
 int
 main (int argc, char *argv[])
@@ -30,191 +68,127 @@ main (int argc, char *argv[])
 	ModestUI         *modest_ui      = NULL;
 
 	GError *err = NULL;
-	int retval  = 0;
-
-	static gboolean update, debug, reinstall;
-	static gchar *mailto, *subject, *bcc, *cc, *body;
+	int retval  = MODEST_ERR_NONE;
+		
+	static gboolean update, debug, batch;
+	static gchar    *mailto, *subject, *bcc, *cc, *body;
 
 	static GOptionEntry options[] = {
 		{ "debug",  'd', 0, G_OPTION_ARG_NONE, &debug,
 		  "Run in debug mode" },
 		{ "update", 'u', 0, G_OPTION_ARG_NONE, &update,
-		  "Send/receive all accounts and exit"},
+		  "Send/receive active accounts"},
 		{ "mailto", 'm', 0, G_OPTION_ARG_STRING, &mailto,
 		  "Start writing a new email to <addresses>"},
 		{ "subject", 's', 0, G_OPTION_ARG_STRING, &subject,
 		  "Subject for a new mail"},
 		{ "body", 'b', 0, G_OPTION_ARG_STRING, &body,
 		  "Body for a new email"},
-		{ "cc",  0, 0, G_OPTION_ARG_STRING, &cc,
-		  "CC-addresses for a new mail (comma-separated)"},
-		{ "bcc", 0, 0, G_OPTION_ARG_STRING, &bcc,
-		  "BCC-adresses for a new mail (comma-separated)"},
-		{ "reinstall-factory-settings", 0, 0, G_OPTION_ARG_NONE, &reinstall,
-		  "Delete all settings and start over (*DESTRUCTIVE*)"
-		},
+		{ "cc",  'c', 0, G_OPTION_ARG_STRING, &cc,
+		  "Cc: addresses for a new mail (comma-separated)"},
+		{ "bcc", 'd', 0, G_OPTION_ARG_STRING, &bcc,
+		  "Bcc: addresses for a new mail (comma-separated)"},
+		{ "batch", 'b', 0, G_OPTION_ARG_NONE, &batch,
+		  "Run in batch mode (don't show UI)"},
 		{ NULL }
 	};
-
-#ifdef IS_HILDON
-	osso_context_t *osso_context;
-#endif
 
 	g_type_init ();
 
 	context = g_option_context_new (NULL);
 	g_option_context_add_main_entries (context, options, NULL);
-
+	
 	if (!g_option_context_parse (context, &argc, &argv, &err)) {
-		g_printerr ("modest: error in command line parameter(s): %s\n",
-			 err ? err->message : "");
-		retval = 1;
+		g_printerr ("modest: error in command line parameter(s): '%s', exiting\n",
+			    err ? err->message : "");
+		g_error_free (err);
+		retval = MODEST_ERR_OPTIONS;
 		goto cleanup;
 	}
-
-	if (debug) {
-		g_log_set_always_fatal (G_LOG_LEVEL_WARNING);
-	}
+	g_option_context_free (context);
 
 	modest_conf = MODEST_CONF(modest_conf_new());
 	if (!modest_conf) {
-		g_warning ("failed to initialize config system");
-		goto cleanup;
-	}
-
-	gtk_init (&argc, &argv);
-
-	modest_icon_factory_init ();
-
-	if (reinstall) {
-		modest_conf_remove_key (modest_conf, MODEST_CONF_NAMESPACE, NULL);
-		install_basic_conf_settings (modest_conf);
-		goto cleanup;
-	}
-
-	modest_ui = MODEST_UI(modest_ui_new (modest_conf));
-	if (!modest_ui) {
-		g_warning ("failed to initialize ui");
+		g_printerr ("modest: failed to initialize config system, exiting\n");
+		retval = MODEST_ERR_CONF;
 		goto cleanup;
 	}
 	
-#ifdef IS_HILDON
-	/* Initialize maemo application */
-	osso_context = osso_initialize(APPLICATION_DBUS_SERVICE, "0.1", TRUE, NULL);
-
-	/* Check that initialization was ok */
-	if (osso_context == NULL)
-		goto cleanup;
-#endif
-
-	{
-		gboolean ok;
-		gtk_init (&argc, &argv);
-
-		if (mailto||cc||bcc||subject||body) {
-#if 0
-			ok = modest_ui_new_edit_window (modest_ui,
-							 mailto,  /* to */
-							 cc,      /* cc */
-							 bcc,     /* bcc */
-							 subject,    /* subject */
-							 body,    /* body */
-							 NULL);   /* attachments */
-#endif
-		} else
-			ok = modest_ui_show_main_window (modest_ui);
-
-		if (!ok)
-			g_warning ("showing window failed");
-		else
-			gtk_main();
-	}
-
+	gtk_init (&argc, &argv);
+	retval = start_ui (modest_conf, mailto, cc, bcc, subject, body);
 
 cleanup:
-	if (err)
-		g_error_free (err);
-
-	if (context)
-		g_option_context_free (context);
-
-	if (modest_ui)
-		g_object_unref (modest_ui);
-
-	if (modest_conf)
-		g_object_unref (modest_conf);
-
-	modest_icon_factory_uninit ();
-
+	g_object_unref (modest_conf);
+	
 	return retval;
 }
 
 
-
-static void
-install_basic_conf_settings (ModestConf *conf)
+int
+start_ui (ModestConf *conf, const gchar* mailto, const gchar *cc, const gchar *bcc,
+	  const gchar* subject, const gchar *body)
 {
-	g_return_if_fail (conf);
+	ModestUI *modest_ui;
+	gint ok, retval = 0;
 
-	/* main window size */
-	modest_conf_set_int (conf, MODEST_CONF_MAIN_WINDOW_WIDTH,
-			     MODEST_CONF_MAIN_WINDOW_WIDTH_DEFAULT, NULL);
-	modest_conf_set_int (conf, MODEST_CONF_MAIN_WINDOW_HEIGHT,
-			     MODEST_CONF_MAIN_WINDOW_HEIGHT_DEFAULT, NULL);
+	modest_ui = MODEST_UI(modest_ui_new (conf));
+	if (!modest_ui) {
+		g_printerr ("modest: failed to initialize ui, exiting\n");
+		retval = MODEST_ERR_UI;
+		goto cleanup;
+	}
+	
+	modest_icon_factory_init ();	
 
-	/* edit window size */
-	modest_conf_set_int (conf, MODEST_CONF_EDIT_WINDOW_WIDTH,
-			     MODEST_CONF_EDIT_WINDOW_WIDTH_DEFAULT, NULL);
-	modest_conf_set_int (conf, MODEST_CONF_EDIT_WINDOW_HEIGHT,
-			     MODEST_CONF_EDIT_WINDOW_HEIGHT_DEFAULT, NULL);
+	if (!hildon_init ()) { /* NOP  if hildon is not defined */
+		g_printerr ("modest: failed to initialize hildon, exiting\n");
+		retval = MODEST_ERR_HILDON;
+		goto cleanup;
+	}
 
-	g_print ("modest: returned to factory settings\n");
+	if (mailto||cc||bcc||subject||body) {
+
+/* 		ok = modest_ui_new_edit_window (modest_ui, */
+/* 						mailto,  /\* to *\/ */
+/* 						cc,      /\* cc *\/ */
+/* 						bcc,     /\* bcc *\/ */
+/* 						subject,    /\* subject *\/ */
+/* 		 				body,    /\* body *\/ */
+/* 						NULL);   /\* attachments *\/ */
+	} else
+		ok = modest_ui_show_main_window (modest_ui);
+	
+	if (!ok) {
+		g_printerr ("modest: showing window failed");
+		retval = MODEST_ERR_RUN;
+		goto cleanup;
+	}	
+	
+	gtk_main();
+	
+cleanup:
+	if (modest_ui)
+		g_object_unref (modest_ui);
+
+	modest_icon_factory_uninit ();
+	return retval;
 }
 
 
-static void
-install_test_account (ModestConf *conf)
+static gboolean
+hildon_init ()
 {
-	ModestAccountMgr *acc_mgr;
-	ModestIdentityMgr *id_mgr;
-	const gchar *acc_name = "test";
-	g_return_if_fail (conf);
+#ifdef MODEST_ENABLE_HILDON
 
-	acc_mgr = MODEST_ACCOUNT_MGR(modest_account_mgr_new (conf));
-	if (!acc_mgr) {
-		g_warning ("failed to instantiate account mgr");
-		return;
+	osso_context_t *osso_context =
+		osso_initialize(PACKAGE, PACKAGE_VERSION,
+				TRUE, NULL);
+	
+	if (!osso_context) {
+		g_printerr ("modest: failed to aquire osso context, exiting");
+		return FALSE;
 	}
+#endif /* MODEST_ENABLE_HILDON */
 
-	if (modest_account_mgr_account_exists (acc_mgr, acc_name, NULL)) {
-		if (!modest_account_mgr_remove_account(acc_mgr, acc_name, NULL)) {
-			g_warning ("could not delete existing account");
-		}
-	}
-
-	if (!modest_account_mgr_add_account (acc_mgr, acc_name, "mystore", "mytransport", NULL))
-		g_warning ("failed to add test account");
-	else
-	{
-		modest_account_mgr_add_server_account (acc_mgr, "mystore", "localhost", "djcb",
-						       NULL, "imap");
-		modest_account_mgr_add_server_account (acc_mgr, "mytransport", "localhost", NULL,
-						       NULL, "smtp");
-
-	}
-	id_mgr = MODEST_IDENTITY_MGR(modest_identity_mgr_new (conf));
-	if (modest_identity_mgr_identity_exists(id_mgr, "myidentity", NULL)) {
-		if (!modest_identity_mgr_remove_identity(id_mgr, "myidentity", NULL)) {
-			g_warning ("could not delete existing identity");
-		}
-	}
-	if (!modest_identity_mgr_add_identity (id_mgr,
-					       MODEST_IDENTITY_DEFAULT_IDENTITY,
-					       "Default User",
-					       "user@localhost",
-					       "", "", FALSE, NULL, FALSE ))
-		g_warning ("failed to add test identity");
-
-	g_object_unref (G_OBJECT(acc_mgr));
-	g_object_unref (G_OBJECT(id_mgr));
+	return TRUE;
 }
