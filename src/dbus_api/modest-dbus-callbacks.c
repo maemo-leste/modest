d0cf371d5b1cfaab8d61f526ba6707f86472973e
Murray Cumming <murrayc@murrayc.com>
2007-04-25 09:54:58 +0000

    2007-04-25  Murray Cumming  <murrayc@murrayc.com>
    * src/dbus_api/modest-dbus-callbacks.c: Added uri_unescape(),
            and uri_parse_mailto() utility functions.
            (on_idle_mail_to): Parse, unescape, and use the subject, cc, bcc, and body items in
            the mailto URI.
            * tests/dbus_api/test_mail_to.c: (main): Add escaped spaces in the URI to test this.
    
    pmo-trunk-r1663

--- src/dbus_api/modest-dbus-callbacks.c
+++ src/dbus_api/modest-dbus-callbacks.c
@@ -34,7 +34,9 @@
 #include "modest-tny-account.h"
 #include "widgets/modest-msg-edit-window.h"
 #include "modest-tny-msg.h"
+#include <libgnomevfs/gnome-vfs-utils.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct 
 {
@@ -141,6 +143,114 @@ static gint on_send_mail(GArray * arguments, gpointer data, osso_rpc_t * retval)
  	return OSSO_OK;
 }
 
+/** uri_unescape:
+ * @uri An escaped URI. URIs should always be escaped.
+ * @len The length of the @uri string, or -1 if the string is null terminated.
+ * 
+ * Decode a URI, or URI fragment, as per RFC 1738.
+ * http://www.ietf.org/rfc/rfc1738.txt
+ * 
+ * Return value: An unescaped string. This should be freed with g_free().
+ */
+static gchar* uri_unescape(const gchar* uri, size_t len)
+{
+	if (!uri)
+		return NULL;
+		
+	if (len == -1)
+		len = strlen (uri);
+	
+	/* Allocate an extra string so we can be sure that it is null-terminated,
+	 * so we can use gnome_vfs_unescape_string().
+	 * This is not efficient. */
+	gchar * escaped_nullterminated = g_strndup (uri, len);
+	gchar *result = gnome_vfs_unescape_string (escaped_nullterminated, NULL);
+	g_free (escaped_nullterminated);
+	
+	return result;
+}
+
+/** uri_parse_mailto:
+ * @mailto A mailto URI, with the mailto: prefix.
+ * @list_items_and_values: A pointer to a list that should be filled with item namesand value strings, 
+ * with each name item being followed by a value item. This list should be freed with g_slist_free) after 
+ * all the string items have been freed. This parameter may be NULL.
+ * Parse a mailto URI as per RFC2368.
+ * http://www.ietf.org/rfc/rfc2368.txt
+ * 
+ * Return value: The to address, unescaped. This should be freed with g_free().
+ */
+static gchar* uri_parse_mailto (const gchar* mailto, GSList** list_items_and_values)
+{
+	const gchar* start_to = NULL;
+	/* Remove the mailto: prefix: 
+	 * 7 is the length of "mailto:": */
+	if (strncmp (mailto, "mailto:", 7) == 0) {
+		start_to = mailto + 7;
+	}
+	
+	if (!start_to)
+		return NULL;
+	
+	/* Look for ?, or the end of the string, marking the end of the to address: */
+	const size_t len_to = strcspn (start_to, "?");
+	gchar* result_to = uri_unescape (start_to, len_to);
+	printf("debug: result_to=%s\n", result_to);
+	
+	/* Get any other items: */
+	const size_t len_mailto = strlen (start_to);
+	const gchar* p = start_to + len_to + 1; /* parsed so far. */
+	const gchar* end = start_to + len_mailto;
+	/* GSList *items = NULL; */
+	const gchar* start_item_name = p;
+	size_t len_item_name = 0;
+	const gchar* start_item_value = NULL;
+	while (p < end) {
+		
+		/* Looking for the end of a name; */
+		if (start_item_name) {
+			const size_t len = strcspn (p, "="); /* Returns whole string if none found. */
+			if (len) {
+				/* This marks the end of a name and the start of the value: */
+				len_item_name = len;
+				
+				/* Skip over the name and mark the start of the value: */
+				p += (len + 1); /* Skip over the = */
+				start_item_value = p;
+			}
+		}
+		
+		/* Looking for the end of a value: */
+		if (start_item_value) {
+			const size_t len = strcspn (p, "?"); /* Returns whole string if none found. */
+			/* ? marks the start of a new item: */
+			if (len) {
+				if (start_item_name && len_item_name) {
+					/* Finish the previously-started item: */
+					gchar *item_value = uri_unescape (start_item_value, len);
+					gchar *item_name = g_strndup (start_item_name, len_item_name);
+					/* printf ("debug: item name=%s, value=%s\n", item_name, item_value); */
+					
+					/* Append the items to the list */
+					if(list_items_and_values) {
+						*list_items_and_values = g_slist_append (*list_items_and_values, item_name);
+						*list_items_and_values = g_slist_append (*list_items_and_values, item_value);
+					}
+				}
+				
+				/* Skip over the value and mark the start of a possible new name/value pair: */
+				p += (len + 1); /* Skip over the ? */
+				start_item_name = p;
+				len_item_name = 0;
+				start_item_value = NULL;
+			}
+		}
+		
+	}
+	
+	return result_to;
+}
+
 
 static gboolean
 on_idle_mail_to(gpointer user_data)
@@ -148,6 +258,8 @@ on_idle_mail_to(gpointer user_data)
 	/* This is based on the implemenation of main.c:start_uil(): */
 	
 	gchar *uri = (gchar*)user_data;
+	GSList *list_names_and_values = NULL;
+	gchar *to = uri_parse_mailto (uri, &list_names_and_values);
 	
 	/* Get the TnyTransportAccount so we can instantiate a mail operation: */
  	ModestAccountMgr *account_mgr = modest_runtime_get_account_mgr();
@@ -171,13 +283,45 @@ on_idle_mail_to(gpointer user_data)
 		if (!from) {
 			g_printerr ("modest: no from address for account '%s'\n", account_name);
 		} else {
-			TnyMsg *msg  = modest_tny_msg_new (uri /* mailto */, from, 
-				NULL /* cc */, NULL /* bcc */,
-				NULL /* subject */, NULL /* body */, NULL /* attachments */);
+			const gchar *cc = NULL;
+			const gchar *bcc = NULL;
+			const gchar *subject = NULL;
+			const gchar *body = NULL;
+			
+			/* Get the relevant items from the list: */
+			GSList *list = list_names_and_values;
+			while (list) {
+				const gchar * name = (const gchar*)list->data;
+				GSList *list_value = g_slist_next (list);
+				const gchar * value = (const gchar*)list_value->data;
+				
+				if (strcmp (name, "cc") == 0) {
+					cc = value;
+				} else if (strcmp (name, "bcc") == 0) {
+					bcc = value;
+				} else if (strcmp (name, "subject") == 0) {
+					subject = value;
+				} else if (strcmp (name, "body") == 0) {
+					body = value;
+				}
+				
+				/* Go to the next pair: */
+				if (list_value) {
+					list = g_slist_next (list_value);
+				} else 
+					list = NULL;
+			}
+			
+			/* Create the message: */
+			TnyMsg *msg  = modest_tny_msg_new (to, from, 
+				cc, bcc, subject, body, 
+				NULL /* attachments */);
+				
 			if (!msg) {
 				g_printerr ("modest: failed to create message\n");
 			} else
 			{
+				/* Add the message to a folder and show its UI for editing: */
 				TnyFolder *folder = modest_tny_account_get_special_folder (account,
 									TNY_FOLDER_TYPE_DRAFTS);
 				if (!folder) {
@@ -194,14 +338,22 @@ on_idle_mail_to(gpointer user_data)
 			
 				g_object_unref (G_OBJECT(msg));
 			}
+			
 			g_object_unref (G_OBJECT(account));
-		
 		}
  	}
  	
  	g_free (account_name);
+ 	
+ 	/* Free the list, as required by the uri_parse_mailto() documentation: */
+ 	if (list_names_and_values)
+ 		g_slist_foreach (list_names_and_values, (GFunc)g_free, NULL);
+ 	g_slist_free (list_names_and_values);
+ 	
+ 	g_free(to);
 		
 	g_free(uri);
+
 	return FALSE; /* Do not call this callback again. */
 }
 
