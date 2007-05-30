09a856ec19c064af7daceb987b9a60dc7dace5f0
Christian Kellner <ckellner@openismus.com>
2007-05-30 13:27:07 +0000

    2007-05-30  Christian Kellner  <ckellner@openismus.com>
    * configure.ac:
    	Search and check for libogs.
    
    	* src/Makefile.am:
    	Incooparte the ogs build env.
    
    	* src/modest-mime-part-search-stream.c:
    	* src/modest-mime-part-search-stream.h:
    	Removed since we do the real searching now in modest-search.c.
    
    	* src/modest-search.c:
    	* src/modest-search.h:
    	Implement support for ogs. We use that if available and requested
    	and fall back to modest_text_utils_utf8_strcmp (). The search logic
    	should now handle the corner case when the search term is spread
    	across multiple reads.
    	Also the logic in modest_search () was corrected to search all
    	requested fields until one is found.
    
    pmo-trunk-r1998

--- src/modest-search.c
+++ src/modest-search.c
@@ -1,5 +1,9 @@
 #define _GNU_SOURCE
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <string.h>
 
 #include <tny-shared.h>
@@ -8,8 +12,10 @@
 #include <tny-iterator.h>
 #include <tny-simple-list.h>
 
+#include "modest-text-utils.h"
+
 #include "modest-search.h"
-#include "modest-mime-part-search-stream.h"
+
 
 static GList*
 add_header (GList *list, TnyHeader *header, TnyFolder *folder)
@@ -21,6 +27,184 @@ add_header (GList *list, TnyHeader *header, TnyFolder *folder)
 	return g_list_prepend (list, str);
 }
 
+static gboolean
+read_chunk (TnyStream *stream, char *buffer, gsize count, gsize *nread)
+{
+	gsize _nread;
+	gssize res;
+
+	_nread = 0;
+	while (_nread < count) {
+		res = tny_stream_read (stream,
+				       buffer + _nread, 
+				       count - _nread);
+		if (res == -1) {
+			*nread = _nread;
+			return FALSE;
+		}
+
+		if (res == 0)
+			break;
+
+		_nread += res;
+	}
+
+	*nread = _nread;
+	return TRUE;
+
+
+}
+
+#ifdef MODEST_HAVE_OGS
+static gboolean
+search_mime_part_ogs (TnyMimePart *part, ModestSearch *search)
+{
+	TnyStream *stream;
+	char       buffer[4096];
+	gsize      len;
+	gsize      nread;
+	gboolean   is_html = FALSE;
+	gboolean   found;
+	gboolean   res;
+
+
+	if (! tny_mime_part_content_type_is (part, "text/ *") ||
+	    ! (is_html = tny_mime_part_content_type_is (part, "text/html"))) {
+		return FALSE;
+	}
+
+	found = FALSE;
+	len = sizeof (buffer);
+	stream = tny_mime_part_get_stream (part);
+
+	while ((res = read_chunk (stream, buffer, len, &nread))) {
+
+		if (is_html) {
+
+			found = ogs_text_searcher_search_html (search->text_searcher,
+							       buffer,
+							       nread,
+							       nread < len);
+		} else {
+			found = ogs_text_searcher_search_text (search->text_searcher,
+							       buffer,
+							       nread);
+		}
+
+		if (found) {
+			break;
+		}
+
+	}
+
+	if (!found) {
+		found = ogs_text_searcher_search_done (search->text_searcher);
+	}
+
+	ogs_text_searcher_reset (search->text_searcher);
+
+	return found;
+}
+#endif
+
+static gboolean
+search_mime_part_strcmp (TnyMimePart *part, ModestSearch *search)
+{
+	TnyStream *stream;
+	char       buffer[8193];
+	char      *chunk[2];
+	gsize      len;
+	gssize     nread;
+	gboolean   found;
+	gboolean   res;
+
+	if (! tny_mime_part_content_type_is (part, "text/ *")) {
+		return FALSE;
+	}
+
+	found = FALSE;
+	len = (sizeof (buffer) - 1) / 2;
+
+	if (strlen (search->body) > len) {
+		g_warning ("Search term bigger then chunk."
+			   "We might not find everything");	
+	}
+
+	stream = tny_mime_part_get_stream (part);
+
+	memset (buffer, 0, sizeof (buffer));
+	chunk[0] = buffer;
+	chunk[1] = buffer + len;
+
+	res = read_chunk (stream, chunk[0], len, &nread);
+
+	if (res == FALSE) {
+		goto done;
+	}
+
+	found = !modest_text_utils_utf8_strcmp (search->body,
+						buffer,
+						TRUE);
+	if (found) {
+		goto done;
+	}
+
+	/* This works like this:
+	 * buffer: [ooooooooooo|xxxxxxxxxxxx|\0] 
+	 *          ^chunk[0]  ^chunk[1]
+	 * we have prefilled chunk[0] now we always read into chunk[1]
+	 * and then move the content of chunk[1] to chunk[0].
+	 * The idea is to prevent not finding search terms that are
+	 * spread across 2 reads:	 
+	 * buffer: [ooooooooTES|Txxxxxxxxxxx|\0] 
+	 * We should catch that because we always search the whole
+	 * buffer not only the chunks.
+	 *
+	 * Of course that breaks for search terms > sizeof (chunk)
+	 * but sizeof (chunk) should be big enough I guess (see
+	 * the g_warning in this function)
+	 * */	
+	while ((res = read_chunk (stream, chunk[1], len, &nread))) {
+		buffer[len + nread] = '\0';
+
+		found = !modest_text_utils_utf8_strcmp (search->body,
+							buffer,
+							TRUE);
+
+		if (found) {
+			break;
+		}
+
+		/* also move the \0 */
+		g_memmove (chunk[0], chunk[1], len + 1);
+	}
+
+done:
+	g_object_unref (stream);
+	return found;
+}
+
+static gboolean
+search_string (const char      *what,
+	       const char      *where,
+	       ModestSearch    *search)
+{
+	gboolean found;
+#ifdef MODEST_HAVE_OGS
+	if (search->flags & MODEST_SEARCH_USE_OGS) {
+		found = ogs_text_searcher_search_text (search->text_searcher,
+					   	       where,
+					   	       strlen (where));
+
+		ogs_text_searcher_reset (search->text_searcher);
+	} else {
+#endif
+		found = !modest_text_utils_utf8_strcmp (what, where, TRUE);
+#ifdef MODEST_HAVE_OGS
+	}
+#endif
+	return found;
+}
 
 
 /**
@@ -36,14 +220,33 @@ modest_search (TnyFolder *folder, ModestSearch *search)
 {
 	GList *retval = NULL;
 	TnyIterator *iter;
-	TnyList *list = tny_simple_list_new ();
+	TnyList *list;
+	gboolean (*part_search_func) (TnyMimePart *part, ModestSearch *search);
+
+	part_search_func = search_mime_part_strcmp;
+
+#ifdef MODEST_HAVE_OGS
+	if (search->flags & MODEST_SEARCH_USE_OGS &&
+	    search->text_searcher == NULL && search->query != NULL) {
+		OgsTextSearcher *text_searcher;	
+
+		text_searcher = ogs_text_searcher_new (FALSE);
+		ogs_text_searcher_parse_query (text_searcher, search->query);
+		search->text_searcher = text_searcher;
+
+		part_search_func = search_mime_part_ogs;
+	}
+#endif
+
+	list = tny_simple_list_new ();
 	tny_folder_get_headers (folder, list, FALSE, NULL);
 
 	iter = tny_list_create_iterator (list);
-	while (!tny_iterator_is_done (iter))
-	{
+
+	while (!tny_iterator_is_done (iter)) {
 		TnyHeader *cur = (TnyHeader *) tny_iterator_get_current (iter);
 		time_t t = tny_header_get_date_sent (cur);
+		gboolean found = FALSE;
 
 		if (search->flags & MODEST_SEARCH_BEFORE)
 			if (!(t <= search->before))
@@ -53,71 +256,85 @@ modest_search (TnyFolder *folder, ModestSearch *search)
 			if (!(t >= search->after))
 				goto go_next;
 
-
 		if (search->flags & MODEST_SEARCH_SIZE)
 			if (tny_header_get_message_size  (cur) < search->minsize)
 				goto go_next;
-		
+
 		if (search->flags & MODEST_SEARCH_SUBJECT) {
-			if (strcasestr (tny_header_get_subject (cur), search->subject))
-				retval = add_header (retval, cur, folder);
-		} else if (search->flags & MODEST_SEARCH_SENDER) {
-			if (strcasestr (tny_header_get_from (cur), search->subject))
-				retval = add_header (retval, cur, folder);
-		} else if (search->flags & MODEST_SEARCH_RECIPIENT) {
-			if (strcasestr (tny_header_get_to (cur), search->subject))
-				retval = add_header (retval, cur, folder);
-		} else if (search->flags & MODEST_SEARCH_BODY) 
-		{
-			TnyHeaderFlags flags = tny_header_get_flags (cur);
-		       	
-			if (flags & TNY_HEADER_FLAG_CACHED)
-			{
-				GError *err = NULL;
-				TnyMsg *msg = tny_folder_get_msg (folder, cur, &err);
-				
-				if (err == NULL)
-				{
-					TnyIterator *piter;
-					TnyList *parts = tny_simple_list_new ();
-					tny_mime_part_get_parts  (TNY_MIME_PART (msg), parts);
-					piter = tny_list_create_iterator (parts);
+			const char *str = tny_header_get_subject (cur);
 
-					while (!tny_iterator_is_done (piter))
-					{
-						TnyStream *stream;
-						TnyMimePart *pcur = (TnyMimePart *) tny_iterator_get_current (piter);
-
-						stream = modest_mime_part_search_stream_new (search->body);
-
-						tny_mime_part_decode_to_stream (pcur, stream);
-
-						if (((ModestMimePartSearchStream *) stream)->found)
-							retval = add_header (retval, cur, folder);				
-
-						g_object_unref (stream);
-						g_object_unref (pcur);
-						tny_iterator_next (piter);
-					}
-					g_object_unref (piter);
-					g_object_unref (parts);
-				} else
-					g_error_free (err);
-				
-				if (msg)
-					g_object_unref (msg);
+			if ((found = search_string (search->subject, str, search))) {
+			    retval = add_header (retval, cur, folder);
 			}
 		}
+		
+		if (!found && search->flags & MODEST_SEARCH_SENDER) {
+			const char *str = tny_header_get_from (cur);
 
-			
+			if ((found = search_string (search->from, str, search))) {
+				retval = add_header (retval, cur, folder);
+			}
+		}
+		
+		if (!found && search->flags & MODEST_SEARCH_RECIPIENT) {
+			const char *str = tny_header_get_to (cur);
 
+			if ((found = search_string (search->recipient, str, search))) {
+				retval = add_header (retval, cur, folder);
+			}
+		}
+		
+		if (!found && search->flags & MODEST_SEARCH_BODY) {
+			TnyHeaderFlags flags;
+			GError      *err = NULL;
+			TnyMsg      *msg = NULL;
+			TnyIterator *piter;
+			TnyList     *parts;
+
+			flags = tny_header_get_flags (cur);
+
+			if (!(flags & TNY_HEADER_FLAG_CACHED)) {
+				goto go_next;
+			}
+
+			msg = tny_folder_get_msg (folder, cur, &err);
+
+			if (err != NULL || msg == NULL) {
+				g_warning ("Could not get message\n");
+				g_error_free (err);
+
+				if (msg) {
+					g_object_unref (msg);
+				}
+			}	
+
+			parts = tny_simple_list_new ();
+			tny_mime_part_get_parts (TNY_MIME_PART (msg), parts);
+
+			piter = tny_list_create_iterator (parts);
+			while (!found && !tny_iterator_is_done (piter)) {
+				TnyMimePart *pcur = (TnyMimePart *) tny_iterator_get_current (piter);
+
+				if ((found = part_search_func (pcur, search))) {
+					retval = add_header (retval, cur, folder);				
+				}
+
+				g_object_unref (pcur);
+				tny_iterator_next (piter);
+			}
+
+			g_object_unref (piter);
+			g_object_unref (parts);
+			g_object_unref (msg);
+
+		}
 go_next:
 		g_object_unref (cur);
 		tny_iterator_next (iter);
 	}
+
 	g_object_unref (iter);
 	g_object_unref (list);
-	
 	return retval;
 }
 
--- src/modest-search.h
+++ src/modest-search.h
@@ -1,6 +1,19 @@
 #ifndef MODEST_SEARCH_H
 #define MODEST_SEARCH_H
 
+#include <glib.h>
+#include <tny-folder.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef MODEST_HAVE_OGS
+#include <libogs/ogs-text-searcher.h>
+#endif
+
+G_BEGIN_DECLS
+
 typedef enum {
 	MODEST_SEARCH_SUBJECT   = (1 << 0),
 	MODEST_SEARCH_SENDER    = (1 << 1),
@@ -8,7 +21,8 @@ typedef enum {
 	MODEST_SEARCH_SIZE 	= (1 << 3),
 	MODEST_SEARCH_BEFORE    = (1 << 4),
 	MODEST_SEARCH_AFTER     = (1 << 5),
-	MODEST_SEARCH_BODY      = (1 << 6)
+	MODEST_SEARCH_BODY      = (1 << 6),
+	MODEST_SEARCH_USE_OGS   = (1 << 7),
 } ModestSearchFlags;
 
 typedef struct {
@@ -16,9 +30,15 @@ typedef struct {
 	time_t before, after;
 	guint minsize;
 	ModestSearchFlags flags;
+#ifdef MODEST_HAVE_OGS
+	const gchar     *query;
+	OgsTextSearcher *text_searcher;	
+#endif
 } ModestSearch;
 
 GList * modest_search (TnyFolder *folder, ModestSearch *search);
+
+G_END_DECLS
 
 #endif
 
