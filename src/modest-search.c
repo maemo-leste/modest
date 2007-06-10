062e0e6106ebe3983e8217fbd898f9c3e61c82d1
Dirk-Jan C. Binnema <dirk-jan.binnema@nokia.com>
2007-06-10 18:06:21 +0000

    * add (c)-headers, forgotten g_object_unref
    pmo-trunk-r2140

--- src/modest-search.c
+++ src/modest-search.c
@@ -1,3 +1,32 @@
+/* Copyright (c) 2006, Nokia Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the Nokia Corporation nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
@@ -24,10 +53,10 @@
 #include "modest-search.h"
 #include "modest-runtime.h"
 
-static char *
-g_strdup_or_null (const char *str)
+static gchar *
+g_strdup_or_null (const gchar *str)
 {
-	char *string = NULL;
+	gchar *string = NULL;
 
 	if  (str != NULL) {
 		string = g_strdup (str);
@@ -147,7 +176,7 @@ search_mime_part_ogs (TnyMimePart *part, ModestSearch *search)
 
 	return found;
 }
-#endif
+#endif /*MODEST_HAVE_OGS*/
 
 static gboolean
 search_mime_part_strcmp (TnyMimePart *part, ModestSearch *search)
@@ -478,6 +507,7 @@ modest_search_all_accounts (ModestSearch *search)
 	}
 
 	g_object_unref (accounts);
+	g_object_unref (iter);
 
 	return hits;
 }
