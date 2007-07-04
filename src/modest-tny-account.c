0bdc7e2da8179a5cffd10d11f2339f547acc0a81
Murray Cumming <murrayc@murrayc.com>
2007-07-04 12:51:40 +0000

    2007-07-04  Murray Cumming <murrayc@murrayc.com>
    * src/modest-tny-account.c: (on_connection_status_changed):
    Commented out the call to modest_platform_connect_and_wait() because this
    seems to always hang, and I would like to know if this is the only thing
    that causes a hang when showing the connection dialog.
    It is not very useful anyway.
    
    pmo-trunk-r2562

--- src/modest-tny-account.c
+++ src/modest-tny-account.c
@@ -167,7 +167,12 @@ on_connection_status_changed (TnyAccount *account, TnyConnectionStatus status, g
 		 * because tinymail does not guarantee that this signal handler will be called 
 		 * in the main thread.
 		 */
-		modest_platform_connect_and_wait (NULL);
+		/* TODO: Commented out (and setting offline instead), because this causes hangs, probably related to 
+		 * our use of mainloops:
+		 * modest_platform_connect_and_wait (NULL);
+		 */
+		 TnyDevice *device = modest_runtime_get_device ();
+		 tny_device_force_offline (device);
 	} else if (status == TNY_CONNECTION_STATUS_CONNECTED_BROKEN) {
 		printf ("DEBUG: %s: Connection broken. Forcing TnyDevice offline.\n", 
 			__FUNCTION__);
