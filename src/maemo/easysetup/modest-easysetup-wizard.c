27dc1b7db4b741b4d7bd0acf233a8c402f172012
Johannes Schmid <johannes.schmid@openismus.com>
2007-07-16 17:17:13 +0000

    2007-07-16  Johannes Schmid <johannes.schmid@openismus.com>
    * src/maemo/easysetup/modest-easysetup-wizard.c:
    	Added scrollbars to account wizard dialog.
    	Fixed projects.maemo.org NB #6274
    
    pmo-trunk-r2743

--- src/maemo/easysetup/modest-easysetup-wizard.c
+++ src/maemo/easysetup/modest-easysetup-wizard.c
@@ -371,10 +371,18 @@ create_page_account_details (ModestEasysetupWizardDialog *self)
 {
 	GtkWidget *box = gtk_vbox_new (FALSE, MODEST_MARGIN_NONE);
 	GtkWidget *label = gtk_label_new(_("mcen_ia_accountdetails"));
+	GtkWidget *scrollwin = gtk_scrolled_window_new(NULL, NULL);
+
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrollwin),
+																 GTK_POLICY_NEVER,
+																 GTK_POLICY_AUTOMATIC);
+
+	
 	gtk_label_set_line_wrap (GTK_LABEL (label), TRUE);
 	gtk_label_set_max_width_chars (GTK_LABEL (label), 40);
 	gtk_box_pack_start (GTK_BOX (box), label, FALSE, FALSE, MODEST_MARGIN_HALF);
 	gtk_widget_show (label);
+	
 	
 	/* Create a size group to be used by all captions.
 	 * Note that HildonCaption does not create a default size group if we do not specify one.
@@ -502,14 +510,23 @@ create_page_account_details (ModestEasysetupWizardDialog *self)
 					      on_entry_max, self);
 	
 	gtk_widget_show (GTK_WIDGET (box));
+	gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(scrollwin),
+																				box);
+	gtk_widget_show (GTK_WIDGET(scrollwin));
 	
-	return GTK_WIDGET (box);
+	return GTK_WIDGET (scrollwin);
 }
 
 static GtkWidget*
 create_page_user_details (ModestEasysetupWizardDialog *self)
 {
 	GtkWidget *box = gtk_vbox_new (FALSE, MODEST_MARGIN_NONE);
+	GtkWidget *scrollwin = gtk_scrolled_window_new(NULL, NULL);
+
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrollwin),
+																 GTK_POLICY_NEVER,
+																 GTK_POLICY_AUTOMATIC);
+
 	
 	/* Create a size group to be used by all captions.
 	 * Note that HildonCaption does not create a default size group if we do not specify one.
@@ -605,8 +622,11 @@ create_page_user_details (ModestEasysetupWizardDialog *self)
 	
 	
 	gtk_widget_show (GTK_WIDGET (box));
+	gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(scrollwin), 
+																				box);
+	gtk_widget_show(scrollwin);
 	
-	return GTK_WIDGET (box);
+	return GTK_WIDGET (scrollwin);
 }
 
 static GtkWidget* create_page_complete_easysetup (ModestEasysetupWizardDialog *self)
@@ -634,6 +654,7 @@ static GtkWidget* create_page_complete_easysetup (ModestEasysetupWizardDialog *s
 	gtk_widget_show (label);
 	
 	gtk_widget_show (GTK_WIDGET (box));
+	
 	return GTK_WIDGET (box);
 }
 
@@ -696,7 +717,13 @@ static void on_entry_incoming_servername_changed(GtkEntry *entry, gpointer user_
 static GtkWidget* create_page_custom_incoming (ModestEasysetupWizardDialog *self)
 {
 	GtkWidget *box = gtk_vbox_new (FALSE, MODEST_MARGIN_NONE);
+	GtkWidget *scrollwin = gtk_scrolled_window_new(NULL, NULL);
 
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrollwin),
+																 GTK_POLICY_NEVER,
+																 GTK_POLICY_AUTOMATIC);
+
+	
 	/* Show note that account type cannot be changed in future: */
 	GtkWidget *label = gtk_label_new (_("mcen_ia_emailsetup_account_type"));
 	gtk_label_set_line_wrap (GTK_LABEL (label), TRUE);
@@ -780,8 +807,11 @@ static GtkWidget* create_page_custom_incoming (ModestEasysetupWizardDialog *self
 	gtk_widget_show (caption);
 	
 	gtk_widget_show (GTK_WIDGET (box));
+	gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW(scrollwin), 
+																				 box);
+	gtk_widget_show(scrollwin);
 	
-	return GTK_WIDGET (box);
+	return GTK_WIDGET (scrollwin);
 }
 
 static void
@@ -839,6 +869,12 @@ static void on_entry_outgoing_servername_changed (GtkEntry *entry, gpointer user
 static GtkWidget* create_page_custom_outgoing (ModestEasysetupWizardDialog *self)
 {
 	GtkWidget *box = gtk_vbox_new (FALSE, MODEST_MARGIN_NONE);
+	GtkWidget *scrollwin = gtk_scrolled_window_new(NULL, NULL);
+
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrollwin),
+																 GTK_POLICY_NEVER,
+																 GTK_POLICY_AUTOMATIC);
+
 	
 	/* Create a size group to be used by all captions.
 	 * Note that HildonCaption does not create a default size group if we do not specify one.
@@ -932,8 +968,10 @@ static GtkWidget* create_page_custom_outgoing (ModestEasysetupWizardDialog *self
 	
 	
 	gtk_widget_show (GTK_WIDGET (box));
+	gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW(scrollwin), box);
+	gtk_widget_show(scrollwin);
 	
-	return GTK_WIDGET (box);
+	return GTK_WIDGET (scrollwin);
 }
 
 static gboolean
@@ -984,6 +1022,7 @@ static GtkWidget* create_page_complete_custom (ModestEasysetupWizardDialog *self
 {
 	GtkWidget *box = gtk_vbox_new (FALSE, MODEST_MARGIN_NONE);
 	GtkWidget *label = gtk_label_new(_("mcen_ia_emailsetup_setup_complete"));
+	
 	gtk_misc_set_alignment (GTK_MISC (label), 0, 0.5);
 	gtk_box_pack_start (GTK_BOX (box), label, FALSE, FALSE, 0);
 	gtk_widget_show (label);
