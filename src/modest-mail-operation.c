61d5de94447c792234d2b34fbc26290362adbce1
Jose Dapena Paz <jdapena@igalia.com>
2007-07-17 15:51:27 +0000

    * src/modest-mail-operation.c: 	* Now it checks if there's no account already, and then cancel cannot 	  be run. Avoids a crash.
    pmo-trunk-r2749

--- src/modest-mail-operation.c
+++ src/modest-mail-operation.c
@@ -417,7 +417,8 @@ modest_mail_operation_cancel (ModestMailOperation *self)
 	/* Cancel the mail operation. We need to wrap it between this
 	   start/stop operations to allow following calls to the
 	   account */
-	tny_account_cancel (priv->account);
+	if (priv->account)
+		tny_account_cancel (priv->account);
 
 	return canceled;
 }
