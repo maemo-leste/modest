4982bdf985bda2b4c414481ddaba2b6d328bd068
Sergio Villar Senin <svillar@igalia.com>
2007-10-19 16:37:27 +0000

    * Added a dbus test that updates (Send&Receive) the default account
    pmo-trunk-r3541

---
+++ tests/dbus_api/test_update_account.c
@@ -0,0 +1,20 @@
+#include <libmodest-dbus-client/libmodest-dbus-client.h>
+#include <stdio.h>
+
+int
+main (int argc, char *argv[])
+{
+	osso_context_t *osso_context;
+
+	osso_context = osso_initialize ("test_update_account",
+					"0.0.1",
+					TRUE,
+					NULL);
+	       
+	if (osso_context == NULL) {
+		g_printerr ("osso_initialize() failed.\n");
+	    return -1;
+	}
+
+	return libmodest_dbus_client_send_and_receive (osso_context);
+}
