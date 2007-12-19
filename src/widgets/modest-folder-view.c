0ab472a38b64b435db369fdf96f92815aa933c58
Sergio Villar Senin <svillar@igalia.com>
2007-12-19 15:19:44 +0000

    * Use the async function instead of the sync call
    pmo-trunk-r3944

--- src/widgets/modest-folder-view.c
+++ src/widgets/modest-folder-view.c
@@ -1126,8 +1126,7 @@ modest_folder_view_finalize (GObject *obj)
 
 	if (priv->cur_folder_store) {
 		if (TNY_IS_FOLDER(priv->cur_folder_store))
-			tny_folder_sync (TNY_FOLDER(priv->cur_folder_store), FALSE, NULL);
-		        /* FALSE --> expunge the message */
+			tny_folder_sync_async (TNY_FOLDER(priv->cur_folder_store), FALSE, NULL, NULL, NULL);
 
 		g_object_unref (priv->cur_folder_store);
 		priv->cur_folder_store = NULL;
