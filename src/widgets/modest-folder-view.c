21769473d57f9c64cececc8c7857c97de7bb6f6c
Sergio Villar Senin <svillar@igalia.com>
2008-02-20 10:39:30 +0000

    * Fixes NB#80896, fixes an endless reconnection against Welho.com
    pmo-trunk-r4205

--- src/widgets/modest-folder-view.c
+++ src/widgets/modest-folder-view.c
@@ -1192,22 +1192,6 @@ modest_folder_view_set_account_store (TnyAccountStoreView *self, TnyAccountStore
 }
 
 static void
-on_connection_status_changed (TnyAccount *self, 
-			      TnyConnectionStatus status, 
-			      gpointer user_data)
-{
-	/* If the account becomes online then refresh it */
-	if (status == TNY_CONNECTION_STATUS_CONNECTED) {
-		const gchar *acc_name;
-		GtkWidget *my_window;
-
-		my_window = gtk_widget_get_ancestor (GTK_WIDGET (user_data), MODEST_TYPE_WINDOW);
-		acc_name = modest_tny_account_get_parent_modest_account_name_for_server_account (self);
-		modest_ui_actions_do_send_receive (acc_name, FALSE, MODEST_WINDOW (my_window));
-	}
-}
-
-static void
 on_account_inserted (TnyAccountStore *account_store, 
 		     TnyAccount *account,
 		     gpointer user_data)
@@ -1237,12 +1221,6 @@ on_account_inserted (TnyAccountStore *account_store,
 	/* Insert the account in the model */
 	tny_list_append (TNY_LIST (gtk_tree_model_sort_get_model (GTK_TREE_MODEL_SORT (sort_model))),
 			 G_OBJECT (account));
-
-
-	/* When the store account gets online refresh it */
-	g_signal_connect (account, "connection_status_changed", 
-			  G_CALLBACK (on_connection_status_changed), 
-			  MODEST_FOLDER_VIEW (user_data));
 
 	/* Refilter the model */
 	gtk_tree_model_filter_refilter (GTK_TREE_MODEL_FILTER (filter_model));
