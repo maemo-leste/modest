c1a90c249afda120478951d65df16ef94e6f6ff3
Jose Dapena Paz <jdapena@igalia.com>
2008-07-22 09:21:26 +0000

    * src/maemo/modest-msg-edit-window.c: 	* Now we properly preserve if the text is formatted on opening 	  a drafts message (fixes NB#87006).
    pmo-trunk-r5089

--- src/maemo/modest-msg-edit-window.c
+++ src/maemo/modest-msg-edit-window.c
@@ -1213,7 +1213,7 @@ set_msg (ModestMsgEditWindow *self, TnyMsg *msg, gboolean preserve_is_rich)
 	if (preserve_is_rich && !is_html) {
 		wp_text_buffer_enable_rich_text (WP_TEXT_BUFFER (priv->text_buffer), FALSE);
 	/* Get the default format required from configuration */
-	} else if (!modest_conf_get_bool (modest_runtime_get_conf (), MODEST_CONF_PREFER_FORMATTED_TEXT, NULL)) {
+	} else if (!preserve_is_rich && !modest_conf_get_bool (modest_runtime_get_conf (), MODEST_CONF_PREFER_FORMATTED_TEXT, NULL)) {
 		wp_text_buffer_enable_rich_text (WP_TEXT_BUFFER (priv->text_buffer), FALSE);
 	}
 
