1b6b2bc255890852d29733c4a9e2508dbb43dcc4
Vivek Sekar <ext-vivek.1.sekar@nokia.com>
2008-11-12 10:27:48 +0000

    added string.h to includes, to avoid the strlen implicit declaration warning
    pmo-trunk-r6277

--- src/modest-server-account-settings.c
+++ src/modest-server-account-settings.c
@@ -30,6 +30,7 @@
 #include <modest-protocol-registry.h>
 #include <modest-server-account-settings.h>
 #include <strings.h>
+#include <string.h>
 
 /* 'private'/'protected' functions */
 static void   modest_server_account_settings_class_init (ModestServerAccountSettingsClass *klass);
