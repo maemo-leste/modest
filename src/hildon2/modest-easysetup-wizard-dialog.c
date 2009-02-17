52127c81621e664656307d57809460f35320986a
Jose Dapena Paz <jdapena@igalia.com>
2009-02-17 12:20:23 +0000

    Restore the size request for labels in wizard complete page.
    pmo-trunk-r7525

--- src/hildon2/modest-easysetup-wizard-dialog.c
+++ src/hildon2/modest-easysetup-wizard-dialog.c
@@ -1011,12 +1011,14 @@ create_page_complete_custom (ModestEasysetupWizardDialog *self)
 	GtkWidget *button_edit = gtk_button_new_with_label (_("mcen_fi_advanced_settings"));
 	hildon_gtk_widget_set_theme_size (button_edit, HILDON_SIZE_FINGER_HEIGHT | HILDON_SIZE_AUTO_WIDTH);
 	gtk_misc_set_alignment (GTK_MISC (label), 0, 0.5);
+	gtk_widget_set_size_request (label, LABELS_WIDTH, -1);
 	gtk_label_set_line_wrap (GTK_LABEL (label), TRUE);
 	gtk_box_pack_start (GTK_BOX (box), label, FALSE, FALSE, 0);
 	gtk_widget_show (label);
 	
 	label = gtk_label_new (_("mcen_ia_customsetup_complete"));
 	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.0);
+	gtk_widget_set_size_request (label, LABELS_WIDTH, -1);
 	gtk_label_set_line_wrap (GTK_LABEL (label), TRUE);
 	gtk_box_pack_start (GTK_BOX (box), label, TRUE, TRUE, 0);
 	gtk_widget_show (label);
