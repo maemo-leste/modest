2a1b5c89e712a8da294ffdfdc72066edd862bedb
Jose Dapena Paz <jdapena@igalia.com>
2009-03-13 10:45:38 +0000

    Be more verbose with comments on what modest-clean.sh does
    pmo-trunk-r8003

--- src/hildon2/modest-clean.sh
+++ src/hildon2/modest-clean.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# First we kill Modest to make sure that, when we remove
+# all the data, modest won't write again
 PIDS=`pidof modest` && kill -15 $PIDS
 
-
+# Removing modest user data folder
 rm -rf /home/user/.modest
 
 exit 0
