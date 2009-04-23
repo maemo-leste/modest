7b7780d493dfdf17473b9589e1bbc8938a060226
Sergio Villar Senin <svillar@igalia.com>
2009-04-23 16:02:36 +0000

    Get the device name from the bluetooth name
    pmo-trunk-r9099

--- src/hildon2/modest-maemo-utils.c
+++ src/hildon2/modest-maemo-utils.c
@@ -31,7 +31,6 @@
 #define DBUS_API_SUBJECT_TO_CHANGE
 #endif /*DBUS_API_SUBJECT_TO_CHANGE*/
 
-#include <sys/utsname.h>
 #include <dbus/dbus.h>
 #include <dbus/dbus-glib-lowlevel.h>
 #include <glib.h>
@@ -92,16 +91,108 @@ modest_maemo_utils_set_osso_context (osso_context_t *osso_context)
 	__osso_context = osso_context;
 }
 
+static void
+update_device_name_from_msg (DBusMessage *message)
+{
+	DBusError error;
+	DBusMessageIter iter;
+
+	dbus_error_init (&error);
+
+	if (dbus_set_error_from_message (&error, message)) {
+		g_printerr ("modest: failed to get bluetooth name: %s\n", error.message);
+		dbus_error_free (&error);
+	} else {
+		const gchar *device_name;
+		if (!dbus_message_iter_init (message, &iter)) {
+			g_printerr ("modest: message did not have argument\n");
+			return;
+		}
+		dbus_message_iter_get_basic (&iter, &device_name);
+		modest_conf_set_string (modest_runtime_get_conf(),
+					MODEST_CONF_DEVICE_NAME, device_name,
+					NULL);
+	}
+}
+
+
+static void
+on_device_name_received (DBusPendingCall *call, void *user_data)
+{
+	DBusMessage *message;
+
+	g_return_if_fail (dbus_pending_call_get_completed (call));
+
+	message = dbus_pending_call_steal_reply (call);
+	if (!message) {
+		g_printerr ("modest: no reply on device name query\n");
+		return;
+	}
+
+	update_device_name_from_msg (message);
+	dbus_message_unref (message);
+}
+
+
+static DBusHandlerResult
+handle_dbus_signal (DBusConnection *conn, DBusMessage *msg, gpointer data)
+{
+	if (dbus_message_is_signal(msg, BTNAME_SIGNAL_IF, BTNAME_SIG_CHANGED))
+		update_device_name_from_msg (msg);
+
+	return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
+}
+
+
+static void
+get_device_name_from_dbus ()
+{
+	static DBusConnection *conn = NULL;
+	DBusMessage *request;
+	DBusError error;
+	DBusPendingCall *call = NULL;
+
+	dbus_error_init (&error);
+	if (!conn) {
+		conn = dbus_bus_get (DBUS_BUS_SYSTEM, &error);
+		if (!conn) {
+			g_printerr ("modest: cannot get on the dbus: %s: %s\n",
+				    error.name, error.message);
+			dbus_error_free (&error);
+			return;
+		}
+	}
+
+	request = dbus_message_new_method_call (BTNAME_SERVICE, BTNAME_REQUEST_PATH,
+						BTNAME_REQUEST_IF, BTNAME_REQ_GET);
+	if (!request) {
+		/* should we free the connection? */
+		g_printerr ("modest: dbus_message_new_method_call failed\n");
+		return;
+	}
+	dbus_message_set_auto_start (request, TRUE);
+	if (dbus_connection_send_with_reply (conn, request, &call, -1)) {
+		dbus_pending_call_set_notify (call, on_device_name_received,
+					      NULL, NULL);
+		dbus_pending_call_unref (call);
+	}
+	dbus_message_unref (request);
+
+	dbus_connection_setup_with_g_main (conn, NULL);
+	dbus_bus_add_match (conn, BTNAME_MATCH_RULE, &error);
+	if (dbus_error_is_set(&error)) {
+		g_printerr ("modest: dbus_bus_add_match failed: %s\n", error.message);
+		dbus_error_free (&error);
+	}
+
+	if (!dbus_connection_add_filter(conn, handle_dbus_signal, NULL, NULL))
+		g_printerr ("modest: dbus_connection_add_filter failed\n");
+}
+
 void
 modest_maemo_utils_get_device_name (void)
 {
-	struct utsname name;
-
-	if (uname (&name) == 0) {
-		modest_conf_set_string (modest_runtime_get_conf(),
-					MODEST_CONF_DEVICE_NAME, name.nodename,
-					NULL);
-	}
+	get_device_name_from_dbus ();
 }
 
 void
