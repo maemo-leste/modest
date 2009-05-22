33cd5f9486a25d8bf0c73f397a81a952cd906430
Sergio Villar Senin <svillar@igalia.com>
2009-05-22 11:58:41 +0200

    Fixes NB#118129, replaced logical strings for new messages again

--- src/widgets/modest-folder-view.c
+++ src/widgets/modest-folder-view.c
@@ -850,10 +850,10 @@ messages_cell_data  (GtkTreeViewColumn *column,
 
 		if (priv->cell_style == MODEST_FOLDER_VIEW_CELL_STYLE_COMPACT) {
 			if (number > 1) {
-				item_name = g_strdup_printf (drafts?_("mcen_ti_messages"):_("mcen_ti_new_messages_plural"), 
+				item_name = g_strdup_printf (drafts?_("mcen_ti_messages"):_("mcen_va_new_messages"), 
 							     number);
 			} else if (number == 1) {
-				item_name = g_strdup_printf (drafts?_("mcen_ti_message"):_("mcen_ti_new_messages_singular"), 
+				item_name = g_strdup_printf (drafts?_("mcen_ti_message"):_("mcen_va_new_message"), 
 							     number);
 			}
 		} 
