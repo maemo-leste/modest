b370fe7b0360f7c9381fca8e04bef9b117ceef80
Sergio Villar Senin <svillar@igalia.com>
2009-06-05 13:35:22 +0200

    Fixes NB120950, crash in time_changed_cb()
    The problem was that we weren't disconnecting time notifications in datetime formatters when they were finalized

--- src/modest-datetime-formatter.c
+++ src/modest-datetime-formatter.c
@@ -228,6 +228,9 @@ modest_datetime_formatter_finalize   (GObject *obj)
 	priv->gconf_handler = 0;
 	gconf_client_remove_dir (gconf, HILDON2_GCONF_FORMAT_DIR,
 				 NULL);
+
+	/* Disconnect notification */
+	osso_time_set_notification_cb (modest_platform_get_osso_context (), NULL, NULL);
 #endif
 	G_OBJECT_CLASS (parent_class)->finalize (obj);
 }
