2d7c6a08252ee2887f49166b8a9e9c6e7c1382aa
Sergio Villar Senin <svillar@igalia.com>
2009-06-10 11:48:43 +0200

    Do not need to unref tree models, they're automatically destroyed

--- src/hildon2/modest-msg-edit-window.c
+++ src/hildon2/modest-msg-edit-window.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006, Nokia Corporation
+/1* Copyright (c) 2006, Nokia Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
