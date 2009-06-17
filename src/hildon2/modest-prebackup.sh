aa2550dd7c4bd174d8953961d4ff64c3e88c01ad
Jose Dapena Paz <jdapena@igalia.com>
2009-06-17 18:07:47 +0200

    Change the backup section to "comm_and_cal" for email.

--- src/hildon2/modest-prebackup.sh
+++ src/hildon2/modest-prebackup.sh
@@ -2,7 +2,7 @@
 
 rm -rf $HOME/.modest-backup.tar.gz
 
-echo "$*" | grep -q "emails" || exit 0
+echo "$*" | grep -q "comm_and_cal" || exit 0
 
 cd $HOME/
 tar czvf .modest-backup.tar.gz --exclude="*.ev-summary.mmap" --exclude=".modest/images/*" .modest
