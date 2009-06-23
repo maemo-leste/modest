1ef983f3b70606e61341c447158a72da8b12a721
Sergio Villar Senin <svillar@igalia.com>
2009-06-23 17:30:57 +0200

    Do not restore settings when creating a folder view because we always set the visible account later anyway

--- src/hildon2/modest-platform.c
+++ src/hildon2/modest-platform.c
@@ -1801,11 +1801,6 @@ modest_platform_create_folder_view_full (TnyFolderStoreQuery *query, gboolean do
 	modest_folder_view_set_style (MODEST_FOLDER_VIEW (widget),
 				      MODEST_FOLDER_VIEW_STYLE_SHOW_ONE);
 
-	/* Restore settings */
-	modest_widget_memory_restore (modest_runtime_get_conf(), 
-				      G_OBJECT (widget),
-				      MODEST_CONF_FOLDER_VIEW_KEY);
-
 	return widget;
 }
 
