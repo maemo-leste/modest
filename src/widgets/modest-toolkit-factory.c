0a24bfb401ac84a0701e0fb2ae02c46f1ad12972
Jose Dapena Paz <jdapena@igalia.com>
2009-10-29 19:47:45 +0100

    New toolkit factory, to instantiate specific widgets for hildon or gtk

---
+++ src/widgets/modest-toolkit-factory.c
@@ -0,0 +1,77 @@
+/* Copyright (c) 2009, Igalia
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the Nokia Corporation nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <glib/gi18n.h>
+#include <modest-hildon-pannable-area-scrollable.h>
+#include "modest-toolkit-factory.h"
+
+static void modest_toolkit_factory_class_init (ModestToolkitFactoryClass *klass);
+static void modest_toolkit_factory_init (ModestToolkitFactory *self);
+
+/* GObject interface */
+static GtkWidget * modest_toolkit_factory_create_scrollable_default (ModestToolkitFactory *self);
+/* globals */
+static GObjectClass *parent_class = NULL;
+
+G_DEFINE_TYPE    (ModestToolkitFactory,
+		  modest_toolkit_factory,
+		  G_TYPE_OBJECT);
+
+ModestToolkitFactory *
+modest_toolkit_factory_new                            (void)
+{
+    GObject* self = g_object_new (MODEST_TYPE_TOOLKIT_FACTORY, NULL);
+
+    return (ModestToolkitFactory *) self;
+}
+
+static void
+modest_toolkit_factory_class_init (ModestToolkitFactoryClass *klass)
+{
+	parent_class = g_type_class_peek_parent (klass);
+
+	klass->create_scrollable = modest_toolkit_factory_create_scrollable_default;
+}
+
+static void
+modest_toolkit_factory_init (ModestToolkitFactory *self)
+{
+}
+
+GtkWidget *
+modest_toolkit_factory_create_scrollable (ModestToolkitFactory *self)
+{
+	return MODEST_TOOLKIT_FACTORY_GET_CLASS (self)->create_scrollable (self);
+}
+
+static GtkWidget *
+modest_toolkit_factory_create_scrollable_default (ModestToolkitFactory *self)
+{
+	return modest_hildon_pannable_area_scrollable_new ();
+}
---
+++ src/widgets/modest-toolkit-factory.h
@@ -0,0 +1,58 @@
+#ifndef                                         __MODEST_TOOLKIT_FACTORY_H__
+#define                                         __MODEST_TOOLKIT_FACTORY_H__
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define                                         MODEST_TYPE_TOOLKIT_FACTORY \
+                                                (modest_toolkit_factory_get_type())
+
+#define                                         MODEST_TOOLKIT_FACTORY(obj) \
+                                                (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
+                                                MODEST_TYPE_TOOLKIT_FACTORY, ModestToolkitFactory))
+
+#define                                         MODEST_TOOLKIT_FACTORY_CLASS(klass) \
+                                                (G_TYPE_CHECK_CLASS_CAST ((klass), \
+                                                MODEST_TYPE_TOOLKIT_FACTORY, ModestToolkitFactory))
+
+#define                                         MODEST_IS_TOOLKIT_FACTORY(obj) \
+                                                (G_TYPE_CHECK_INSTANCE_TYPE ((obj), MODEST_TYPE_TOOLKIT_FACTORY))
+
+#define                                         MODEST_IS_TOOLKIT_FACTORY_CLASS(klass) \
+                                                (G_TYPE_CHECK_CLASS_TYPE ((klass), MODEST_TYPE_TOOLKIT_FACTORY))
+
+#define                                         MODEST_TOOLKIT_FACTORY_GET_CLASS(obj) \
+                                                (G_TYPE_INSTANCE_GET_CLASS ((obj), \
+                                                MODEST_TYPE_TOOLKIT_FACTORY, ModestToolkitFactoryClass))
+
+typedef struct                                  _ModestToolkitFactory ModestToolkitFactory;
+
+typedef struct                                  _ModestToolkitFactoryClass ModestToolkitFactoryClass;
+
+struct                                          _ModestToolkitFactoryClass
+{
+	GObjectClass parent_class;
+
+	GtkWidget * (*create_scrollable) (ModestToolkitFactory *self);
+};
+
+struct                                          _ModestToolkitFactory
+{
+    GObject parent;
+};
+
+
+GType
+modest_toolkit_factory_get_type                       (void) G_GNUC_CONST;
+
+ModestToolkitFactory *
+modest_toolkit_factory_new                            (void);
+
+GtkWidget *
+modest_toolkit_factory_create_scrollable              (ModestToolkitFactory *self);
+
+G_END_DECLS
+
+#endif /* __MODEST_WP_TEXT_VIEW_H__ */
