9877fbbb86da9d24779f6c151a0c546558d57780
Sergio Villar Senin <svillar@igalia.com>
2009-11-09 10:16:21 +0100

    Do not update the header window if the window was previously closed
    Fixes NB#145788

--- src/hildon2/modest-header-window.c
+++ src/hildon2/modest-header-window.c
@@ -906,6 +906,12 @@ update_view (ModestHeaderWindow *self,
 	TnyFolder *folder;
 
 	g_return_if_fail (MODEST_IS_HEADER_WINDOW(self));
+
+	/* It could happen when some event is received and the window
+	   was previously closed */
+	if (!MODEST_IS_HEADER_WINDOW (self))
+		return;
+
 	priv = MODEST_HEADER_WINDOW_GET_PRIVATE (self);
 
 	folder = modest_header_view_get_folder ((ModestHeaderView *) priv->header_view);
