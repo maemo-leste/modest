294b11c411d496d9365463280b3bb8aca9ac8571
Jose Dapena Paz <jdapena@igalia.com>
2009-12-10 16:34:52 +0100

    Show cancel button in move to dialog

--- src/gtk/modest-platform.c
+++ src/gtk/modest-platform.c
@@ -2174,6 +2174,8 @@ modest_platform_create_move_to_dialog (GtkWindow *parent_window,
 					      GTK_WINDOW (parent_window),
 					      GTK_DIALOG_MODAL | GTK_DIALOG_NO_SEPARATOR |
 					      GTK_DIALOG_DESTROY_WITH_PARENT,
+					      GTK_STOCK_CANCEL,
+					      GTK_RESPONSE_CANCEL,
 					      _FM_CHANGE_FOLDER_NEW_FOLDER,
 					      MODEST_GTK_RESPONSE_NEW_FOLDER,
 	                                      NULL);
