036e2e88da8f0d8129684d146295db8829e074e1
Sergio Villar Senín <svillar@igalia.com>
2010-01-19 13:03:53 +0100

    Show the signature in SecondaryTextColor for HTML emails
    Fixes NB#147995

--- src/modest-text-utils.h
+++ src/modest-text-utils.h
@@ -569,4 +569,6 @@ gchar * modest_text_utils_quote_names (const gchar *recipients);
 
 gboolean modest_text_utils_no_recipient (GtkTextBuffer *buffer);
 
+gchar * modest_text_utils_create_colored_signature (const gchar *signature);
+
 #endif /* __MODEST_TEXT_UTILS_H__ */
