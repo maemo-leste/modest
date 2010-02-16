119d348778de8b60a87e12fe83116201fc0f1b3a
José Dapena Paz <jdapena@igalia.com>
2010-02-16 18:42:39 +0100

    Reorganize the editor menu following new specs:    * Remove undo in menu but keep accelerator    * Remove find in editor    * Restructure menu to reduce times the buttons change the order      they're shown.
    Fix 1/2 for NB#156612

--- src/hildon2/modest-msg-edit-window.c
+++ src/hildon2/modest-msg-edit-window.c
@@ -4480,15 +4480,6 @@ setup_menu (ModestMsgEditWindow *self)
 	priv = MODEST_MSG_EDIT_WINDOW_GET_PRIVATE (self);
 
 	/* Settings menu buttons */
-	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_editor_checknames"), NULL,
-					   APP_MENU_CALLBACK (modest_ui_actions_on_check_names),
-					   NULL);
-	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_viewer_addtocontacts"), NULL,
-					   APP_MENU_CALLBACK (modest_ui_actions_add_to_contacts),
-					   MODEST_DIMMING_CALLBACK (modest_ui_dimming_rules_on_add_to_contacts));
-	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_inbox_undo"), "<Ctrl>z",
-					   APP_MENU_CALLBACK (modest_ui_actions_on_undo),
-					   MODEST_DIMMING_CALLBACK (modest_ui_dimming_rules_on_undo));
 
 	priv->cc_button = hildon_check_button_new (0);
 	gtk_button_set_label (GTK_BUTTON (priv->cc_button), _("mcen_me_editor_showcc"));
@@ -4512,18 +4503,6 @@ setup_menu (ModestMsgEditWindow *self)
 	gtk_button_set_alignment (GTK_BUTTON (priv->bcc_button), 0.5, 0.5);
 	gtk_button_set_alignment (GTK_BUTTON (priv->bcc_button), 0.5, 0.5);
 
-	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_editor_add_attachment"), NULL,
-					   APP_MENU_CALLBACK (modest_msg_edit_window_add_attachment_clicked),
-					   NULL);
-	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_inbox_remove_attachments"), NULL,
-					   APP_MENU_CALLBACK (modest_ui_actions_on_remove_attachments),
-					   MODEST_DIMMING_CALLBACK (modest_ui_dimming_rules_on_editor_remove_attachment));
-	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_message_settings"), NULL,
-					   APP_MENU_CALLBACK (on_message_settings),
-					   NULL);
-	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_viewer_find"), "<Ctrl>f",
-					   APP_MENU_CALLBACK (modest_ui_actions_on_toggle_find_in_page),
-					   NULL);
 	priv->show_toolbar_button = hildon_check_button_new (0);
 	gtk_button_set_label (GTK_BUTTON (priv->show_toolbar_button), _("mcen_bd_show_toolbar"));
 	hildon_check_button_set_active (HILDON_CHECK_BUTTON (priv->show_toolbar_button),
@@ -4535,6 +4514,21 @@ setup_menu (ModestMsgEditWindow *self)
 	gtk_button_set_alignment (GTK_BUTTON (priv->show_toolbar_button), 0.5, 0.5);
 	gtk_button_set_alignment (GTK_BUTTON (priv->show_toolbar_button), 0.5, 0.5);
 
+	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_message_settings"), NULL,
+					   APP_MENU_CALLBACK (on_message_settings),
+					   NULL);
+	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_editor_checknames"), NULL,
+					   APP_MENU_CALLBACK (modest_ui_actions_on_check_names),
+					   NULL);
+	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_editor_add_attachment"), NULL,
+					   APP_MENU_CALLBACK (modest_msg_edit_window_add_attachment_clicked),
+					   NULL);
+	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_inbox_remove_attachments"), NULL,
+					   APP_MENU_CALLBACK (modest_ui_actions_on_remove_attachments),
+					   MODEST_DIMMING_CALLBACK (modest_ui_dimming_rules_on_editor_remove_attachment));
+	modest_hildon2_window_add_to_menu (MODEST_HILDON2_WINDOW (self), _("mcen_me_viewer_addtocontacts"), NULL,
+					   APP_MENU_CALLBACK (modest_ui_actions_add_to_contacts),
+					   MODEST_DIMMING_CALLBACK (modest_ui_dimming_rules_on_add_to_contacts));
 }
 
 static void
