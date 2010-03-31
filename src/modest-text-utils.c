990d64251cce71133f035083bad1ec84fb0cc672
alexander chumakov <alexander.chumakov@teleca.com>
2010-03-31 15:46:54 +0400

    Fixed memory leak in modest-text-utils.c:modest_text_utils_get_addresses_indexes
    Fixed bug: #162742

--- src/modest-text-utils.c
+++ src/modest-text-utils.c
@@ -615,7 +615,6 @@ modest_text_utils_convert_to_html_body (const gchar *data, gssize n, gboolean hy
 void
 modest_text_utils_get_addresses_indexes (const gchar *addresses, GSList **start_indexes, GSList **end_indexes)
 {
-	GString *str;
 	gchar *start, *cur;
 
 	if (!addresses)
@@ -624,7 +623,6 @@ modest_text_utils_get_addresses_indexes (const gchar *addresses, GSList **start_
 	if (strlen (addresses) == 0)
 		return;
 
-	str = g_string_new ("");
 	start = (gchar*) addresses;
 	cur = (gchar*) addresses;
 
